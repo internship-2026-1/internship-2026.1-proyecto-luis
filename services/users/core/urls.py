fbb748931cfeaeaa7a89d714cf7b3152e6960017
Luis Morales <luiscule01@gmail.com>
2026-04-09 16:33:16 -0600

    IP-003, feat Se actualizo docker-compose.yml, nginx.conf y urls.py

--- services/users/core/urls.py
+++ services/users/core/urls.py
@@ -4,5 +4,5 @@ from .views import HelloWorldView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/hello/', HelloWorldView.as_view(), name='hello-world'),
+    path('hello/', HelloWorldView.as_view(), name='hello-world'),
 ]
